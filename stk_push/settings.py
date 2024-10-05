22cf8742a544713bb2de768d7dcf69b848aaafba
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2024-10-05 11:22:49 +0300

    file overriding conflict

--- stk_push/settings.py
+++ stk_push/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-67_z@b&oo^sfgz3%4!jzmty1aly%ty#vpvgoarx9tl=dy!8osn'
+SECRET_KEY = 'django-insecure-8^&0q#_@6kmm2!9ytemft&ds#_fzowp1f%_vsxk6kzeyf__qf)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
